ce219d1b7c6daffd8d39393818d34b3c96b76f84
Faith Chikwekwe <faithchikwekwe01@gmail.com>
2019-06-19 14:06:05 -0700

    🗝 implemented keys and rooms

---
+++ keys_rooms.py
@@ -0,0 +1,24 @@
+
+def can_visit_all_rooms(rooms):
+    graph = {i: val for i, val in enumerate(rooms)}
+    # print(graph)
+    # since nodes are 0 to n - 1, we can start with node 0
+    return dfs(graph, 0)
+
+def dfs(graph, node, visited=set()):
+    if node not in visited:
+        visited.add(node)
+
+        for neighbor in graph[node]:
+            dfs(graph, neighbor, visited)
+
+    # if not all nodes were visited, return False
+    if len(visited) < len(graph):
+        return False
+    return True  # all nodes were visited
+
+if __name__ == "__main__":
+    # uncomment one rooms variable below to test
+    rooms = [[1], [2], [3], []] # true
+    # rooms = [[1, 3], [3, 0, 1], [2], [0]] # false
+    print(can_visit_all_rooms(rooms))
